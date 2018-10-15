fa186197370d5e1d296d47426bff5959344ba7d6
Haylton Rodrigues Costa <haylton2012@gmail.com>
2018-10-15 10:28:02 -0300

    Faltam testes de persistir foto e arquivo

---
+++ src/com/haylton/estudo/model/Foto.java
@@ -0,0 +1,118 @@
+package com.haylton.estudo.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author Haylton
+ */
+@Table(name = "foto")
+@Entity
+public class Foto implements Serializable{
+    @Id
+    @SequenceGenerator(name = "seq_foto", sequenceName = "seq_foto", allocationSize = 1)
+    @GeneratedValue(generator = "seq_foto")
+    private Integer id;
+    
+    @NotNull(message = "O campo nome deve ser preenchido")
+    @NotBlank(message = "O campo nome não pode estar em branco")
+    @Length(max = 50, message = "O nome deve ter no máximo {max} caracteres")
+    @Column(name = "nome", length = 50, nullable = false)
+    private String nome;
+    
+    @NotNull(message = "O campo descrição deve ser preenchido")
+    @NotBlank(message = "O campo descrição não pode estar em branco")
+    @Length(max = 50, message = "A descrição pode conter no máximo {max} caracteres")
+    @Column(name = "descricao", nullable = false, length = 50)
+    private String descricao;
+    @Lob
+    private byte[] arquivo;
+    
+    @NotNull(message = "O produto deve ser informado")
+    @ManyToOne
+    @JoinColumn(name = "produto", referencedColumnName = "id", nullable = false)
+    private Produto produto;
+
+    public Foto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public byte[] getArquivo() {
+        return arquivo;
+    }
+
+    public void setArquivo(byte[] arquivo) {
+        this.arquivo = arquivo;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Foto other = (Foto) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+ 
+}
